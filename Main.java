7361ebb20566fba2ac1268e88b317c71205a3cfd
David Tovar <davidtovar2@u.boisestate.edu>
2018-03-28 22:58:38 -0600

    Revision 2 in branch ([WIP] Start the implementation of task #123)

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main
 {
-	//this is the chronological revision 1 in master
+	//this is the chronological revision 2 in master
 
 	public void sampleMethod()
 	{
 		System.out.println("We'll assume that this change implements task #117");
+		System.out.println("Work in progress...");
+		System.out.println("Starting the implementation of task #123");
 	}
 }
