092275a0dea3a7306f017a7c94ca43fce30860a7
David Tovar <davidtovar2@u.boisestate.edu>
2018-03-28 23:05:48 -0600

    Fixes #128

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main
 {
-	//this is the chronological revision 1 in master
+	//this is the chronological revision 4 in master
 
 	public void sampleMethod()
 	{
 		System.out.println("We'll assume that this change implements task #117");
+		Systme.out.println("We'll assume that this change fixes bug #128");
 	}
 }
