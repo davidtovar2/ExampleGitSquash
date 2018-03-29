8058ff734437281ff7c983ec02393af50e2c5200
David Tovar <davidtovar2@u.boisestate.edu>
2018-03-28 22:49:21 -0600

    Implements #117

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	//this is the chronological revision 1 in master
+
+	public void sampleMethod()
+	{
+		System.out.println("We'll assume that this change implements task #117");
+	}
+}
